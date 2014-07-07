381c24f3d4b002ffa47d90dfa4020637baa1d4b1
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-07 14:55:54 -0700

    Explains radixSort

---
+++ radixSort.js
@@ -0,0 +1,8 @@
+// Radix sort is a noncomparative sort (often for integers)
+// that sorts based on the value of each digit of each value.
+// Starting with either the least significant digit or most significant digit,
+// each value is placed into a bucket based on that value.  The values are then
+// resorted based on the next digit, and so on until all m sorts have been completed
+// where m is the length of the largest number
+// Radix sort is a stable sort, meaning that when multiple values are placed in the same
+// bucket, order is maintained from the previous order
