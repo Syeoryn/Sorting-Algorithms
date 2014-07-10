5463d146ee492454b67c1234ece69ff9d98c3779
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-09 23:49:37 -0700

    Implements inPlaceRadixSort

--- radixSort.js
+++ radixSort.js
@@ -36,3 +36,29 @@ var binaryRadixSort = function(array){
   return array;
 }
 
+var inPlaceRadixSort = function(array){
+  // Determine number of digits in largest number
+  var max = Math.max.apply(null, array);
+  var length = Math.log(max) / Math.log(2);
+  var value;
+  var ones;
+  var i;
+
+  // iterate over every element once for each digit in largest number
+  for(var shiftBy = 0; shiftBy <= length; shiftBy++){
+    // sort each value towards beginning or end of array based on current digit being examined
+    i = 0;
+    ones = 0;
+    while( i + ones < array.length){
+      shifted = array[i] >> shiftBy;
+      if(shifted % 2 === 0){
+        i++;
+      } else {
+        value = array.splice(i, 1)[0];
+        array.push(value);
+        ones++;
+      }
+    }
+  }
+  return array;
+}
