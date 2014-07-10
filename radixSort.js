8a9531dcf7c1c65e030d1261ac64d9ab77d336a6
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-09 22:46:34 -0700

    Implements binaryRadixSort

--- radixSort.js
+++ radixSort.js
@@ -6,3 +6,29 @@
 // where m is the length of the largest number
 // Radix sort is a stable sort, meaning that when multiple values are placed in the same
 // bucket, order is maintained from the previous order
+
+var binaryRadixSort = function(array){
+  var zeroes = [];
+  var ones = [];
+  var shifted;
+
+  var max = Math.max.apply(null, array);
+  var length = Math.log(max) / Math.log(2);
+
+  for(var shiftBy = 0; shiftBy <= length; shiftBy++){
+    for(var i = 0; i < array.length; i++){
+      shifted = array[i] >> shiftBy;
+      if(shifted % 2 === 0) {
+        zeroes.push(array[i]);
+      } else {
+        ones.push(array[i]);
+      }
+    }
+    array = zeroes.concat(ones);
+    zeroes = [];
+    ones = [];
+  }
+
+  return array;
+}
+
