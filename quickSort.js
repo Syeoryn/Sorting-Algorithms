39326f5af30e4fe337839b7e8b9ba561e794b68c
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-11 17:12:30 -0700

    Explains quickSort

---
+++ quickSort.js
@@ -0,0 +1,15 @@
+// Quick sort starts with the value at index 0, treating it as a pivot, 
+// and sorts the array into two chunks: values greater, and values less than the pivot
+// Then, the pivot is placed in the middle of these two chunks, which is it's final position.
+// This is then repeated for the left half and the right half, until all chunks are sorted.
+
+// To sort array into chunks, two pointers are maintained.  A left pointer, which starts at index 1
+// and moves right until a value greater than the pivot is found; and a right pointer, which starts
+// at index length - 1 and moves left until a value less than the pivot is found.  Once the left and 
+// right pivots cross each other (or equal each other), the pivot is swapped with the value one index
+// to the left of the left pointer.  If both pointers stop before equaling each other, the two values
+// the pointers point at are swapped, and the pointers continue progressing until they equal each other.
+// This is done recursively with the base case of an array of length 0 or 1, as these arrays are already
+// sorted.
+
+// Efficient implementations of quick sort are done in place, but are not stable.
