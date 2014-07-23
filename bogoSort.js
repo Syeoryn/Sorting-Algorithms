a6f180a3897cbc87d678c03eabb0664b791bb82b
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-22 23:45:50 -0700

    Implements bogoSort

--- bogoSort.js
+++ bogoSort.js
@@ -1,3 +1,10 @@
 // Bogo sort shuffles the array, then checks if it's sorted
 // If not sorted, it shuffles again.
 // If sorted, it stops
+
+var bogoSort = function(array){
+  while(!isSorted(array)){
+    shuffle(array);
+  }
+  return array;
+}
