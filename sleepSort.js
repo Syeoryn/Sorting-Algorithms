2fa81c05a07954800abdb0579e18c653b2b5b048
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-23 19:32:16 -0700

    Implements sleepSort

--- sleepSort.js
+++ sleepSort.js
@@ -1,2 +1,15 @@
 // Sleep sort sets a timer proportional to each value in the array,
 // then pushes that value to the sorted array when the timer ends
+
+var sleepSort = function(array){
+  var sorted = [];
+  var setTimer = function(number){
+    setTimeout(function(){
+      sorted.push(number);
+    }, number*1);
+  }
+  for(var i = 0; i < array.length; i++){
+    setTimer(array[i]);
+  }
+  return sorted;
+}
