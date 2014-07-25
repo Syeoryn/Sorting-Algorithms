84d31b9bb616601572c86e24a3cbd4453f9ba59d
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-24 23:55:13 -0700

    Describes cocktail sort

---
+++ cocktailSort.js
@@ -0,0 +1,8 @@
+// cocktail sort is a bi-directional bubble sort.
+// Starting at the beginning and iterating forward,
+// the largest value is found and swapped into the last postion.
+// Moving backwards from the end, the smallest value is found
+// and swapped into the first position.
+// This is then repeated to swap the second largest value
+// into the second to last position, and so on 
+// until the array is sorted.
