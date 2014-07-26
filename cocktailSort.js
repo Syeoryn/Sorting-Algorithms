66e5a5010e04c180d72cebe81a7e295fc2a72abc
Drew Cuthbertson <syeoryn@gmail.com>
2014-07-25 23:56:57 -0700

    Implements cocktailSort

--- cocktailSort.js
+++ cocktailSort.js
@@ -6,3 +6,28 @@
 // This is then repeated to swap the second largest value
 // into the second to last position, and so on 
 // until the array is sorted.
+
+var cocktailSort = function(array, start, end){
+  start = start || 1;
+  end = end || array.length;
+  if(start >= end){
+    return array;
+  }
+  for(var i = start; i < end; i++){
+    console.log('up', i);
+    if(array[i - 1] > array[i]){
+      var temp = array[i - 1];
+      array[i - 1] = array[i];
+      array[i] = temp;
+    }
+  }
+  for(var i = end; i > start - 1; i--){
+    console.log('down',i)
+    if(array[i] < array[i - 1]){
+      var temp = array[i - 1];
+      array[i - 1] = array[i];
+      array[i] = temp;
+    }
+  }
+  return cocktailSort(array,start + 1, end -1);
+}
